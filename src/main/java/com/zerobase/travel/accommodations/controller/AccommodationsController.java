c70ea772981e4683b353722bc4c5c55c9448e3f1
KIMMIJIDLQSLEK <meeji222@naver.com>
2023-08-18 04:07:09 +0900

    Fix: AccommodationController todo 작성

--- src/main/java/com/zerobase/travel/accommodations/controller/AccommodationsController.java
+++ src/main/java/com/zerobase/travel/accommodations/controller/AccommodationsController.java
@@ -5,7 +5,6 @@ import com.zerobase.travel.accommodations.dto.AccommodationDto;
 import com.zerobase.travel.accommodations.dto.KeywordDto;
 import com.zerobase.travel.accommodations.service.AccommodationService;
 import lombok.RequiredArgsConstructor;
-import org.apache.ibatis.javassist.compiler.ast.Keyword;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,29 +16,31 @@ import java.util.List;
 @RestController
 public class AccommodationsController {
 
-//    private final AccommodationServiceImpl accommodationServiceImpl;
     private final AccommodationService accommodationService;
 
-    @GetMapping("/list")
-    public ResponseEntity<List<AccommodationDto>> accommodationList(){
-        List<AccommodationDto> accommodationDto=accommodationService.list();
 
-        try{
-            return new ResponseEntity<>(accommodationDto,HttpStatus.OK);
-        }catch(Exception e){
-            return new ResponseEntity<>(accommodationDto,HttpStatus.NOT_FOUND);
+    @GetMapping("/list")
+    public ResponseEntity<List<AccommodationDto>> accommodationList() {
+        List<AccommodationDto> accommodationDto = accommodationService.list();
+
+        //todo: 숙소 전체조회 기능
+        try {
+            return new ResponseEntity<>(accommodationDto, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(accommodationDto, HttpStatus.NOT_FOUND);
         }
 
     }
 
     @PostMapping("/search")
-    public ResponseEntity<List<AccommodationDto>> accommodationSearch(@RequestBody KeywordDto keyword){
+    public ResponseEntity<List<AccommodationDto>> accommodationSearch(@RequestBody KeywordDto keyword) {
         List<AccommodationDto> accommodationDto = accommodationService.searchList(keyword);
 
-        try{
-            return new ResponseEntity<>(accommodationDto,HttpStatus.OK);
-        }catch(Exception e){
-            return new ResponseEntity<>(accommodationDto,HttpStatus.NOT_FOUND);
+        //todo: 숙소 검색 기능
+        try {
+            return new ResponseEntity<>(accommodationDto, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(accommodationDto, HttpStatus.NOT_FOUND);
         }
     }
 
