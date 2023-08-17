f4f148d4e5c275875be768f4cfdda50531ceffb1
KimKyuHyeok <kyuhyeok1011@gmail.com>
2023-08-17 11:31:17 +0900

    jwt 토큰 권한 설정

--- src/main/java/com/zerobase/travel/user/config/SecurityConfig.java
+++ src/main/java/com/zerobase/travel/user/config/SecurityConfig.java
@@ -1,41 +1,68 @@
 package com.zerobase.travel.user.config;
 
+import com.zerobase.travel.user.jwt.JwtAuthenticationFilter;
+import com.zerobase.travel.user.jwt.JwtTokenProvider;
+import com.zerobase.travel.user.jwt.JwtUserService;
+import com.zerobase.travel.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 추가
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserMapper userMapper;
 
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests()
-                .antMatchers("/user/login").permitAll()
-                .antMatchers("/user/testing").permitAll()
-                .antMatchers("/user/signIn").permitAll()
-                .antMatchers("/user/findId").permitAll()
-                .antMatchers("/email/send").permitAll()
-                .antMatchers("/user/test").hasRole("USER")
-                .antMatchers("/error").permitAll()
-                .anyRequest().authenticated()
-                .and();
-        return http.build();
+                    .authorizeRequests()
+                .antMatchers("/user/login", "/user/signUp", "/user/findId").permitAll()
+                .antMatchers("/board").authenticated()
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(final WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/h2-console/**");
+    }
+
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public JwtTokenProvider jwtTokenProvider() throws Exception {
+        return new JwtTokenProvider(memberService());
+    }
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        return new JwtAuthenticationFilter(jwtTokenProvider());
+    }
+    @Bean
+    public JwtUserService memberService() throws Exception {
+        return new JwtUserService(userMapper, passwordEncoder());
     }
 
     @Bean
--- src/main/java/com/zerobase/travel/user/dto/TokenDTO.java
+++ src/main/java/com/zerobase/travel/user/dto/TokenDTO.java
@@ -1,5 +1,6 @@
 package com.zerobase.travel.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 @Builder
@@ -7,7 +8,9 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TokenDTO {
     private String accessToken;
     private String refreshToken;
+    private String message;
 }
